8e5f0a2f83f9001a7aaa8a987fdbafd481700a81
Chi Barca <nguyenhuuchi3006@gmail.com>
2019-04-25 21:27:32 +0700

    hashing password roi moi luu vao db

--- SERVER/models/user.js
+++ SERVER/models/user.js
@@ -1,5 +1,8 @@
 const mongoose=require('mongoose');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
 
     email: {
@@ -41,6 +44,27 @@ const userSchema = mongoose.Schema({
     }
 
 });
+
+userSchema.pre('save', function(next){                  // truoc khi user.save((err,doc)) thi no lam ham nay
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                if(err) return next(err);
+
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+    
+})
 
 const User = mongoose.model('User', userSchema);
 
