63d4c845ed67119cd5f94abb1788fcfedec74077
Chi Barca <nguyenhuuchi3006@gmail.com>
2019-05-02 19:38:09 +0700

    setting redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,23 @@ import ReactDOM from 'react-dom';
 import './Resources/css/styles.css';
 
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';                          // 2 cái này chưa hiểu
+import ReduxThunk from 'redux-thunk';
 
 import Routes from './routes';
+import Reducer from './reducers';
 
-ReactDOM.render(
+const createStoreWithMiddleware =  applyMiddleware(promiseMiddleware,ReduxThunk)(createStore);
 
-<BrowserRouter>
-    <Routes />
-</BrowserRouter>
+ReactDOM.render(                                    //      này để dùng cho redux dev tool trên chrome
+    <Provider store={createStoreWithMiddleware(Reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>
+    
 
 , document.getElementById('root'));
 
